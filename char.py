e20197337a50251f3a6295da21b0f4da04d9085e
eas10 <vvateva@highpoint.edu>
2019-02-19 15:18:15 -0500

    making changes to dictionary, so it parses correctly 2

--- char.py
+++ char.py
@@ -6,23 +6,25 @@ import pandas
 
 
 title='my first chart'
-#
-# sheet = pyexcel.get_sheet(file_name='data.csv')
-#
-# svg = sheet.plot(chart_type='pie',title=title,width=600,height=400, explicit_size='True')
-#
-# #pyexcel.save_as(adict=sheet, dest_title=title,dest_chart_type='pie', dest_file_name='pie.svg', dest_no_prefix=True)
-#
-# svg.save_as('test.sortable.html',display_lenght=10)
+
 
 data=pandas.read_csv('data.csv',index_col=0,squeeze=True).to_dict()
 print(data)
 
 bar_chart=pygal.Bar(style=LightStyle, width=500, height=700, legend_at_bottom=True, human_readable=True, title=title)
 
-value=data.values()
+my_values1=[]
+my_keys1=[]
 
-bar_chart.add(str(list(data.keys())),values=value)
+my_values=data.values()
+my_keys=data.keys()
+print(my_keys)
 
+for value in my_values:
+    my_values1.append(value)
+for key in my_keys:
+    my_keys1.append(key)
 
+for key in my_keys1:
+    bar_chart.add(str(my_keys1),values=my_values1)
 bar_chart.render_to_file('bar_chart.svg')
